86ad98f8109f736e46fa0ced98af594c342089d5
Jason4zh <117495330+Jason4zh@users.noreply.github.com>
2022-12-19 20:02:59 +0800

    Delete src/init directory

--- src/init/edit.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-cd C:\Users\zrz19\Desktop\Python整活\我的pypi包\WheelDecide
-python setup.py sdist build
-twine upload dist/*
-start https://github.com/Jason4zh/WheelDecide/
-start https://pypi.org/project/WheelDecide/
